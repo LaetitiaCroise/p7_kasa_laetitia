1c042dea3700cb73fc1bf59c6354d49c4b0c439f
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-16 11:09:52 +0100

    init project w/ vite

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import kasaDatas from './datas/datas.json'
+
+
+function App() {
+
+ console.log(kasaDatas);
+
+
+
+  return (
+    <div className="App">
+     
+    </div>
+  )
+}
+
+export default App
