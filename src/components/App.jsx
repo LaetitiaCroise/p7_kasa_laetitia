031154d51f49e8dfbecdedb429e9ea92433bb923
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-17 17:11:10 +0100

    reload

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,10 @@
-
 import './App.css'
-import kasaDatas from './datas/datas.json'
+import kasaDatas from '../datas/datas.json'
 
 
 function App() {
 
  console.log(kasaDatas);
-
-
 
   return (
     <div className="App">
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles/index.css'
+import App from './components/App'
+import { createRoot } from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+
+root.render(
+		<App />
+)
