919e29460fa2b522787ddef588c6cdae40bef965
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-17 18:33:02 +0100

    Revert "reload"
    This reverts commit 031154d51f49e8dfbecdedb429e9ea92433bb923.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+
 import './App.css'
-import kasaDatas from '../datas/datas.json'
+import kasaDatas from './datas/datas.json'
 
 
 function App() {
 
  console.log(kasaDatas);
+
+
 
   return (
     <div className="App">
