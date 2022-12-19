a771e723dbab7b6e8a52bdfc7148aef3b8e66d6a
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-19 15:52:45 +0100

    modif css

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ import React from 'react';
 import './styles/index.css'
 import App from './components/App'
 
-// importation  de la méthode {createRoot} avec react dom/client
-import { createRoot } from 'react-dom/client';
+// // importation  de la méthode {createRoot} avec react dom/client
+// import { createRoot } from 'react-dom/client';
 
-//recupère le rootElement dans 'root'
-const rootElement = document.getElementById('root');
+// //recupère le rootElement dans 'root'
+// const rootElement = document.getElementById('root');
 
-// création de la const root = createRoot pour rootElement
-const root = createRoot(rootElement);
+// // création de la const root = createRoot pour rootElement
+// const root = createRoot(rootElement);
 
-root.render(
-		<App />
-)
-// createRoot()contrôle le contenu du nœud de conteneur que vous transmettez. 
-//Tous les éléments DOM existants à l'intérieur sont remplacés lorsque render est appelé. 
-//Les appels ultérieurs utilisent l'algorithme de différenciation DOM de React pour des mises à jour efficaces.
+// // renvoie à l'interieur du 'root' le composant <App/>
+// root.render(
+// 		<App />
+// )
+// // createRoot()contrôle le contenu du nœud de conteneur que vous transmettez. 
+// //Tous les éléments DOM existants à l'intérieur sont remplacés lorsque render est appelé. 
+// //Les appels ultérieurs utilisent l'algorithme de différenciation DOM de React pour des mises à jour efficaces.
+ ReactDOM.render(<App/>, document.getElementById('root'));
