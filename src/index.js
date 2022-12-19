384b828d700b54ba117a636c1b24e3d854600156
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-19 12:59:49 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@
 import React from 'react';
 import './styles/index.css'
 import App from './components/App'
-// importation  de la création 'Root' avec react dom/client
+// importation  de la méthode {createRoot} avec react dom/client
 import { createRoot } from 'react-dom/client';
-
+//recupère le rootElement dans 'root'
 const rootElement = document.getElementById('root');
+// création de la const root = createRoot pour rootElement
 const root = createRoot(rootElement);
 
 root.render(
