b3b5b97c7545c657b422b311f0e37c12ee4dfea2
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-20 12:30:38 +0100

    update Commits

--- src/components/FicheLogement/Rate.js
+++ src/components/FicheLogement/Rate.js
@@ -2,14 +2,16 @@ import React from "react";
 import Star from '../../assets/Star.png'
 import StarEmpty from '../../assets/StarEmpty.png';
 
-// je créé la fonction  Rate avec en paramètre "logement" pour récuperer ensuite son ratonScore allant de 1 à 5 (étoiles)
+// je récupère les props en créant la fonction  Rate avec en paramètre "logement" pour récuperer ensuite son ratonScore allant de 1 à 5 (étoiles)
 function Rate({ logement }) {
 const ratingScore = [1, 2, 3, 4, 5]
 return (
     <div>
             {/* je boucle  sur mon ratingScore, tant que la condition est positive, alors les étoile sont pleine, sinon, elles sont vide */}
-            {ratingScore.map((ratingElem) => // Je passe chaque élement du tableau avec map pour retourné le résultat ( ratingElem)
-                logement >= ratingElem ? (
+            
+            {ratingScore.map((ratingElem) => // Je passe chaque élement du tableau avec map pour retourné le résultat (ratingElem)
+            
+                logement >= ratingElem ? (//  j'attribue la valeur de objet  entre les accolade {ratingElem.toString}
                     <img 
                         src={Star} 
                         key={ratingElem.toString()} 
