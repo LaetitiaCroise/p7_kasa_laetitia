cc4555fee0a0cbce4385fc01824f40cb4384481a
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-29 17:32:54 +0100

    modif css

--- src/components/FicheLogement/Tags.js
+++ src/components/FicheLogement/Tags.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-
 //je prépage ma fonction Tag pour l'importer dans FicheLogement
 function Tag({ logement }) {
 
     const tag = logement.tags;
     const tagMap = tag.map((e, index) => (
+
         <p className="tagParagraphe" key={e}>{logement.tags[index]}</p>
     ));
     return <div className="tag">{tagMap}</div>;
