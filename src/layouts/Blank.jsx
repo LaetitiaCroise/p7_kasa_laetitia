8ab814a6dbc0e75c318d34ce9b7cf38a244d0056
Croisé Laëtitia <110659766+LaetitiaCroise@users.noreply.github.com>
2022-12-30 13:51:07 +0100

    modif Blank

--- src/layouts/Blank.jsx
+++ src/layouts/Blank.jsx
@@ -1,12 +1,14 @@
+import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header'
 
 const LayoutError = () => {
     return (
-        <>
+        // Renvoie deux elements à l'interieur de react Fragment
+        <React.Fragment>
         <Header/>
             <Outlet />
-        </>
+        </React.Fragment>
     );
 };
 
@@ -14,3 +16,6 @@ export default LayoutError;
 
 // Un <Outlet>doit être utilisé dans les éléments de route parents
 // pour rendre leurs éléments de route enfants. Cela permet à l'interface utilisateur imbriquée de s'afficher lorsque les itinéraires enfants sont rendus.
+
+// Outlet Cette bibliothèque fournit un composant de sortie de routeur qui modifie dynamiquement
+// le contenu en fonction de la route actuelle. Il s'agit d'un remplacement pratique pour la combinaison de et de composants de React Router <Switch>
--- src/layouts/Default.jsx
+++ src/layouts/Default.jsx
@@ -1,15 +1,20 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
+import React from 'react';
 
+// pour pouvoir renvoyer mes composant à plusieur endroit je vais proceder à un retrun des composant Header .ect
 const LayoutDefault = () => {
     return (
-        <>
+         // Renvoie les elements à l'interieur de react Fragment
+        <React.Fragment>
         <Header />
         <Outlet />
         <Footer />
-        </>
+        </React.Fragment>
     )
 };
 
 export default LayoutDefault;
+// Un <Outlet>doit être utilisé dans les éléments de route parents
+// pour rendre leurs éléments de route enfants. Cela permet à l'interface utilisateur imbriquée de s'afficher lorsque les itinéraires enfants sont rendus.
